9a3345d275d122eaab1ed35a727e9d8797386b8e
Bruno Menslin <bruno.menslin@totvs.com.br>
2024-10-24 23:33:34 -0300

    refactor: add comments to explain the code

--- artificialNeuralNetwork/perceptron.py
+++ artificialNeuralNetwork/perceptron.py
@@ -1,50 +1,132 @@
+# Perceptron!
+# Forma simples de rede neural artificial, utilizada para classificação binária.
+# Treinado para classificar quatro padrões de entrada, o algoritmo ajusta os
+# pesos até que o erro entre a *saída real* e a *saída desejada* seja pequeno.
+
+    # Objetivo!
+    # Treinar um perceptron (rede neural com um único neurônio) para classificar
+    # um conjunto de entradas em duas classes (1 ou -1).
+
+    # Entradas!
+    # São valores binários expandidos com um bias (sempre -1).
+
+    # Saídas!
+    # Classificação como 1 ou -1.
+
+    # Processo!
+    # A rede ajusta os pesos iterativamente com base no erro até que a diferença
+    # entre as saídas desejadas e as calculadas seja mínima.
+
+
+# Bibliotecas Necessárias!
 import numpy
 
-def f(u):
-    if u >= 0:
-        return 1
+
+# Função de Ativação!
+# Realiza a classificação das saídas em duas classes, A e B.
+def activation(weightedSum):
+    if weightedSum >= 0:
+        return 1 # Classe A "verdadeiro".
     else:
-        return -1
+        return -1 # Classe B "falso".
 
-def findOutput(data, w):
-    u = 0.0
-    for i in range(0, len(data)):
-        u += w[i]*data[i]
-    return f(u)
 
-# initialization
-p = [[1,1,-1],[1,-1,-1],[-1,1,-1],[-1,-1,-1]] # conjunto de valores de entrada ampliados com a entrada dummy
-d = [1, 1, 1, -1] # saidas desejadas
-w = numpy.random.rand(len(p[0])) # inicializacao randomica dos pesos
+# Função de Saída!
+# Calcula a saída, retornando a classificação das entradas e pesos que recebe.
+def findOutput(inputs, weights):
+    weightedSum = 0 # Valor escalar "u".
 
-# learning rate (taxa de aprendizado).
-# deve afetar a velocidade de convergência do algoritmo.
-# quanto menor a taxa de aprendizado, mais iterações.
+    # Calcula a soma ponderada das entradas, multiplicando cada entrada do vetor
+    # "inputs" pelos pesos correspondentes no vetor "weights".
+    # Os pesos são ajustados conforme a taxa de aprendizado.
+    for i in range(0, len(inputs)):
+        weightedSum += (weights[i] * inputs[i])
+
+    # Retorna a classificação das entradas e pesos.
+    return activation(weightedSum)
+
+
+# conjunto de valores de entrada ampliados com a entrada dummy.
+# A entrada é aumentada com um elemento extra fixo para -1 (bias).
+# Bias é uma forma de ajustar a função de ativação para melhor separação linear.
+inputs = [
+    [1, 1, -1], # Par de entradas binárias (x1 e x2) mais o bias.
+    [1, -1, -1], # Conjuntos relacionados a problemas de lógica binária.
+    [-1, 1, -1],
+    [-1, -1, -1]
+]
+
+
+# saidas desejadas
+desiredOutputs = [1, 1, 1, -1]
+
+
+# Pesos!
+# Inicialização randômica dos pesos.
+weights = numpy.random.rand(len(inputs[0]))
+
+
+# Taxa de Aprendizado!
+# Determina a magnitude do ajuste nos pesos a cada iteração.
+# Um valor maior acelera o aprendizado, mas pode causar instabilidade.
+# Um valor menor torna o aprendizado mais lento.
 learningRate = 0.01
 
-# desired error (erro desejado).
-# deve afetar o número de iterações necessárias para convergência do algoritmo.
-# quanto menor o erro desejado, mais iterações
-d_error = 0.001
 
-iter = 0
+# Erro Desejado!
+# Deve afetar o número de iterações necessárias para convergência do algoritmo.
+# O treinamento termina quando o erro acumulado da rede for inferior ao desejado.
+desiredErrors = 0.001
+
+
+# Número de Iterações!
+# A cada época, loop while, a rede neural é treinada com todos os inputs.
+iterations = 0
+
+
+# Laço de Treinamento!
 while True:
-    error = 0
-    for i in range(0, len(p)):
-        o = findOutput(p[i], w)
-        error += ((d[i]-o)**2)/2
-        learningSignal = learningRate*(d[i]-o)
-        for k in range(0, len(p[i])):
-            w[k] += learningSignal*p[i][k]
 
-    iter += 1
-    print(error, " ## ", w)
-    if error < d_error:
-        print('N. iterations:', iter)
+    # Erro Acumulado da Rede!
+    errors = 0.0
+
+    # Loop para as amostras de entrada.
+    for i in range(0, len(inputs)):
+
+        # Para cada amostra de entrada, calcula-se a saída atual.
+        output = findOutput(inputs[i], weights)
+
+        # O erro acumulado representa a diferença quadrática entre a saída
+        # desejada "desiredOutputs" e a saída calculada "output".
+        errors += ((desiredOutputs[i] - output) ** 2) / 2
+
+        # Definição do sinal de aprendizado, que é a diferença entre a saída
+        # desejada "desiredErrors" e a saída atual "output" multiplicada
+        # pela taxa de aprendizado "learningRate".
+        learningSignal = (learningRate * (desiredOutputs[i] - output))
+
+        # Os pesos são ajustados de acordo com o sinal de aprendizado
+        # "learningSignal" para aproximar a saída desejada.
+        for k in range(0, len(inputs[i])):
+            weights[k] += (learningSignal * inputs[i][k])
+
+    # Cálculo de iterações.
+    iterations += 1
+
+    # Registro de treinamento.
+    print(errors, weights)
+
+    # O treinamento continua até que o erro acumulado "errors" seja inferior ao
+    # erro desejado "desiredErrors", o que indica que a rede aprendeu
+    # a classificar corretamente as entradas.
+    if errors < desiredErrors:
+        print('Iterations:', iterations)
         break
 
-print(findOutput([1,1,-1],w))
-print(findOutput([1,-1,-1],w))
-print(findOutput([-1,1,-1],w))
-print(findOutput([-1,-1,-1],w))
-# print result
+
+# Após o treinamento, o código testa o perceptron com as mesmas entradas usadas
+# durante o treinamento para verificar se a rede agora classifica corretamente.
+print(findOutput([1, 1, -1], weights))
+print(findOutput([1, -1, -1], weights))
+print(findOutput([-1, 1, -1], weights))
+print(findOutput([-1, -1, -1], weights))
